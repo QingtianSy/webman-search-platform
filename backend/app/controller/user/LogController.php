8e8032141fe9bfa57775021aea5ee8276ea1ae42
Minis <minis@local>
2026-04-18 00:09:13 +0800

    refactor: move user log queries into user service layer

--- backend/app/controller/user/LogController.php
+++ backend/app/controller/user/LogController.php
@@ -3,11 +3,7 @@
 namespace app\controller\user;
 
 use app\common\CurrentUser;
-use app\common\user\UserListBuilder;
-use app\repository\mysql\BalanceLogRepository;
-use app\repository\mysql\LoginLogRepository;
-use app\repository\mysql\OperateLogRepository;
-use app\repository\mysql\PaymentLogRepository;
+use app\service\user\LogService;
 use app\validate\user\LogQueryValidate;
 use support\ApiResponse;
 use support\Request;
@@ -17,28 +13,24 @@ class LogController
     public function balance(Request $request)
     {
         $query = (new LogQueryValidate())->list($request->all());
-        $list = (new BalanceLogRepository())->listByUserId(CurrentUser::id($request));
-        return ApiResponse::success(UserListBuilder::make($list, $query['page'], $query['page_size']));
+        return ApiResponse::success((new LogService())->balance(CurrentUser::id($request), $query));
     }
 
     public function payment(Request $request)
     {
         $query = (new LogQueryValidate())->list($request->all());
-        $list = (new PaymentLogRepository())->listByUserId(CurrentUser::id($request));
-        return ApiResponse::success(UserListBuilder::make($list, $query['page'], $query['page_size']));
+        return ApiResponse::success((new LogService())->payment(CurrentUser::id($request), $query));
     }
 
     public function login(Request $request)
     {
         $query = (new LogQueryValidate())->list($request->all());
-        $list = (new LoginLogRepository())->listByUserId(CurrentUser::id($request));
-        return ApiResponse::success(UserListBuilder::make($list, $query['page'], $query['page_size']));
+        return ApiResponse::success((new LogService())->login(CurrentUser::id($request), $query));
     }
 
     public function operate(Request $request)
     {
         $query = (new LogQueryValidate())->list($request->all());
-        $list = (new OperateLogRepository())->listByUserId(CurrentUser::id($request));
-        return ApiResponse::success(UserListBuilder::make($list, $query['page'], $query['page_size']));
+        return ApiResponse::success((new LogService())->operate(CurrentUser::id($request), $query));
     }
 }
